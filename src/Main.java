0c953e12270bd0f5b18ebca52cb3c3b452910164
Zavodilin1985 <zavodilin-ru@yandex.ru>
2023-08-10 21:34:25 +0300

    Add logs in feature

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Yess");
         System.out.println("Feature branch");
+        System.out.println("New logs in feature #2");
     }
 }
